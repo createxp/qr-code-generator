3c78e9ed799fa0a1c9843e78a8f6f100e4c27d51
saketkhare <saketkhare2000@gmail.com>
2022-09-27 10:15:03 +0530

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head >
+        <title>QR Code Generator | createxp</title>
+        <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-4xl text-center text-red-500">
+        Hello QR Code
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
