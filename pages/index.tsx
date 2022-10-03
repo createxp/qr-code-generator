7defd055d9ab66cb2e259f53a6eef739cb362aa0
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-10-03 08:29:39 +0530

    added a button in landing page to go to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from "next";
+import { Button } from "../src/components/utility";
 import { publicRoute } from "../src/routes";
+import { AiOutlineQrcode } from 'react-icons/ai'
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const router = useRouter();
   return (
-    <div>
+    <div className="flex flex-col gap-4 w-full h-screen justify-center items-center">
       <h1 className="font-bold text-4xl text-center">
-        Hello QR Code
+        QR Code Generator
       </h1>
-
+      <Button
+        text={'Go to App'}
+        icon={<AiOutlineQrcode />}
+        onClick={() => router.push('/app')}
+      />
     </div>
   );
 };
