7064d36e71d75bb42ab7aca4d3b28c681b736b6d
Trehan Sangpriya <trehansangpriya@gmail.com>
2023-05-09 10:07:59 +0530

    clean up index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,18 @@
 import type { NextPage } from "next";
-import { Button } from "../src/components/utility";
 import { publicRoute } from "../src/routes";
-import { AiOutlineQrcode } from "react-icons/ai";
-import { useRouter } from "next/router";
-import { Faq, Features, Footer, Hero } from "../src/components/pages";
+import { Faq, Features, Hero } from "../src/components/pages";
 import Spacer from "../src/components/utility/spacer";
 import CTA from "../src/components/app/CTA";
 
 const Home: NextPage = () => {
-  const router = useRouter();
   return (
     <div className="">
-      {/* <h1 className="font-bold text-4xl text-center">
-        QR Code Generator
-      </h1>
-      <Button
-        text={'Go to App'}
-        icon={<AiOutlineQrcode />}
-        onClick={() => router.push('/app')}
-      /> */}
       <Hero />
       <Features />
       <Spacer />
       <Faq />
       <Spacer />
       <CTA />
-      {/* <Footer /> */}
-      {/* <Spacer /> */}
     </div>
   );
 };
